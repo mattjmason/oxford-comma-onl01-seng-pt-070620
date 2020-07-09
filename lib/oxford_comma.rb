19bdb13f7e0f967f0f01279297747648b21057eb
Matt Mason <masonmj10@gmail.com>
2020-07-09 05:30:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,8 @@ def oxford_comma(array)
    return "#{array}"
    elsif array.length == 2 
    return array.join(" and ")
+   elsif array.length >= 3
+   array[-1] = "and #{array[-1]}"
+   return array.join(",")
   end
 end
