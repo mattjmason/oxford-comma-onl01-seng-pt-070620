ecfcc6711c48407aa885a59cffb6fd57e1d9eeed
Matt Mason <masonmj10@gmail.com>
2020-07-09 05:19:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,4 +2,5 @@ def oxford_comma(array)
   ["kiwi"].join
   ["kiwi", "durian"].join("and")
   ["kiwi", "durian", "starfruit"].join(",","and")
+  ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"].join(",", "and")
 end
